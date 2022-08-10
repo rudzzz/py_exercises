6f335799aba1305620c1acd9820e6157817c0d5c
rudzzz <rudsolza13@gmail.com>
2022-08-09 21:59:25 -0300

    descobre a quantidade de caracteres em uma string

---
+++ contador_caracteres.py
@@ -0,0 +1,7 @@
+def str_counter():
+    """Função que retorna o tamanho de uma sting"""
+    string = input("Digite uma string: ")
+    tamanho = len(string)
+    print(f"O tamanho da string é de {tamanho} caracteres")
+
+str_counter()
